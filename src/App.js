b8b7d9ae2250ee76faa36f60db73765d74f165f6
BinhGiauTen <binhpro50@gmail.com>
2024-10-09 00:41:58 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import MainLayout from './components/MainLayout';
+import Coaches from './pages/Coaches';
+import Users from './pages/Users';
+import Courses from './pages/Courses';
+import AddCourse from './pages/AddCourse';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route  path="/" element={<Login/>}/>
+        <Route  path="/admin" element={<MainLayout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path="coaches" element={<Coaches/>}/>
+          <Route path="users" element={<Users/>}/>
+          <Route path="courses" element={<Courses/>}/>
+          <Route path="course" element={<AddCourse/>}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <h3 className="mb-4 title">Dashboard</h3>
+    </>
+  );
+};
+
+export default Dashboard;
